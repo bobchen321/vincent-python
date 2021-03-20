bb328fca177362a07b14bbe12a8a317f4bb7b89e
unknown <104179@stu.jkes.tp.edu.tw>
2021-03-20 17:42:11 +0800

    homework 3

---
+++ class/3/grade.py
@@ -0,0 +1,12 @@
+score=input("please input score")
+socre=int(score)
+if (score >=90):
+    print("a")
+elif (score >=80):
+    print("b")
+elif (score >=70):
+    print("c")
+elif (score >=60):
+    print("d")
+else  (score <=59):
+    print("e")
---
+++ class/3/password.py
@@ -0,0 +1,8 @@
+name=input("please input name")
+password=input("please input password")
+color=input("please input color")
+
+if (password == '1234') & (name == 'vincent') & (color == red )
+   print("true")
+else:
+    nrint("false")
